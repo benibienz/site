c532a49d5b60565d35028bf43dedd19fe6486a70
Beni Bienz <benibienz@gmail.com>
2020-11-06 01:57:59 -0700

    7b

--- fwd/src/App.js
+++ fwd/src/App.js
@@ -1,54 +1,107 @@
+import React, { useState } from "react";
 import "./App.css";
 import {
   makeStyles,
+  Button,
   Card,
   Checkbox,
   ListItem,
   ListItemIcon,
   ListItemText,
   TextField,
+  IconButton,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 // Material UI styles hook
 const useStyles = makeStyles({
   root: {
-    width: "20%",
+    width: "20em",
     margin: "3em",
     padding: "2em",
+  },
+  add: {
+    margin: "1em",
   },
 });
 
 // TODO item with checkbox, text and delete icon
-const TodoItem = ({ itemText }) => (
-  <ListItem>
-    <ListItemIcon>
-      <Checkbox edge="start" />
-    </ListItemIcon>
-    <ListItemText primary={itemText} />
-    <DeleteIcon />
-  </ListItem>
-);
+const TodoItem = ({ itemText, handleDelete }) => {
+  return (
+    <ListItem>
+      <ListItemIcon>
+        <Checkbox edge="start" />
+      </ListItemIcon>
+      <ListItemText primary={itemText} />
+      <IconButton onClick={handleDelete}>
+        <DeleteIcon />
+      </IconButton>
+    </ListItem>
+  );
+};
 
 // Text field to add a new item
-const AddItemField = () => (
-  <TextField variant="filled" label="Add new item"></TextField>
-);
+const AddItemField = ({ addItem }) => {
+  const [itemText, setItemText] = useState("");
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(itemText);
+    setItemText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        variant="filled"
+        label="Add new item"
+        value={itemText}
+        onChange={(e) => setItemText(e.target.value)}
+      />
+      <Button
+        className={classes.add}
+        type="submit"
+        variant="contained"
+        color="primary"
+        disabled={!itemText}
+      >
+        Add
+      </Button>
+    </form>
+  );
+};
 
 // Item count display
-const ItemCount = () => <p>Item count: 3</p>;
+const ItemCount = ({ count }) => <p>Item count: {count}</p>;
 
 // Main container for TODO list
 const TodoList = () => {
   const classes = useStyles();
+  const [items, setItems] = useState([]);
+  const [id, setId] = useState(0);
+
+  const addItem = (text) => {
+    setItems((items) => [
+      ...items,
+      <TodoItem
+        itemText={text}
+        key={id}
+        id={id}
+        handleDelete={() =>
+          setItems((items) => items.filter((item) => item.props.id !== id))
+        }
+      />,
+    ]);
+    setId((id) => id + 1);
+  };
+
   return (
-    <Card maxWidth="sm" className={classes.root}>
+    <Card maxwidth="sm" className={classes.root}>
       <h1>TODO List</h1>
-      <TodoItem itemText="Make mockup" />
-      <TodoItem itemText="Make static app" />
-      <TodoItem itemText="Represent state" />
-      <AddItemField />
-      <ItemCount />
+      {items}
+      <AddItemField addItem={addItem} />
+      <ItemCount count={items.length} />
     </Card>
   );
 };
